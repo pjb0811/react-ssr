eb73682440d26a60cbdc9acebd8b6111116029dc
jbpark <jbpark@wemakeprice.com>
2018-11-08 11:41:42 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,7 @@
+require('@babel/polyfill');
+require('@babel/register')({
+  ignore: [/\/(build|node_modules)\//],
+  presets: ['@babel/preset-env', '@babel/preset-react']
+});
+
+require('./server');
