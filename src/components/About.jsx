8b2708f66162a4b3345c97bbe52cc5e3d6416947
jbpark <pjb0811@gmail.com>
2019-02-17 21:03:48 +0900

    updating head settings

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import withLayout from './withLayout';
 import Counter from './Counter';
+import { Helmet } from 'react-helmet';
 
 const About = () => {
   return (
     <div>
+      <Helmet>
+        <title>About</title>
+        <meta name="description" content="about" />
+      </Helmet>
       <h1>About</h1>
       <Counter />
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import withLayout from './withLayout';
 import Counter from './Counter';
-import { Helmet } from 'react-helmet';
 
 const Home = () => {
   return (
     <div>
-      <Helmet>
-        <title>Home</title>
-        <meta name="description" content="counter" />
-      </Helmet>
       <h1>home</h1>
       <Counter />
     </div>
